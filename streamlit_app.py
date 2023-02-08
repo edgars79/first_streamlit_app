b949cf482595f1508842be7c635a2a2527091d52
edgars.dumins <edgars.dumins@accenture.com>
2023-02-08 08:33:20 +0200

    Added requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import pandas
 import streamlit
+import requests
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -11,6 +12,11 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
+
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -18,4 +24,7 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
